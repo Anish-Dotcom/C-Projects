3c300acf2b4e49fcf3772d0688e442d97ee15d06
U-BSD\460470 <460470@SHS-BYBF6X3.beaverton.k12.or.us>
2025-10-07 11:34:34 -0700

    Almost done tictactoe

--- tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe.cpp
@@ -9,23 +9,29 @@ bool checkwin(int player, int table[3][3]);
 int main() {
   int table[3][3] = {{0,0,0},{0,0,0},{0,0,0}};// Creates a 3x3 array that holds the tictactoe board
   bool playing = true;
-  char input[2];
+  char input[3];
   int blank = 0;
   int xMove =1;
   int oMove =2;
   int xWins =0;
   int oWins =0;
-  int turn = 0;
+  int turn = 1;
   while (playing) {
-      while(checkwin(turn ,table)== false) {
+      cout << "Type move in coordinate form Ex. b3" << endl;
+      for(int i = 0; i < 3; i++) {
+	for(int j = 0; j < 3; j++) {
+	  table[i][j] = 0; 
+        }
+      }
+      while(checkwin(1,table)== false && checkwin(2, table) == false) {
+	
 	int row =0;
 	int col =0;
 	cout << endl;
-	cout << "Type move in coordinate form Ex. b3" << endl;
 	printboard(table);
-	cout << "Player " << turn+1 << " enter your move: ";
+	cout << "Player " << turn << " enter your move: ";
 	cin.get(input,3);
-	if(strlen(input) == 2) {
+	if(strlen(input) == 3) {
 	  cout<< "Please type a valid input STRLEN" << endl;
 	} else if (input[0] != 'a' && input[0] != 'b' && input[0] != 'c') {
 	  cout<< "Please type a valid input ABC" << endl;
@@ -34,18 +40,31 @@ int main() {
 	} else {
 	  row = (int)(input[0]) -97;
 	  col = input[1]-49;
-	  cout << row << col << endl;
-	  table[row][col] = turn;
-	  if(turn == 0) {
-	    turn = 1;
+	  if(table[row][col] != 0) {
+	    cout << "Square taken" << endl;
 	  } else {
-	    turn = 0;
+	    table[row][col] = turn;
+	    if(turn == 1) {
+	      turn = 2;
+	    } else {
+	      turn = 1;
+	    }
 	  }
 	}
         
 	cin.ignore();
       }
-      playing = false;
+      cout << endl;
+      cout << "Player "<< turn << " wins!" << endl;
+      if(turn == 1) {
+	xWins++;
+      } else {
+	oWins++;
+      }
+      cout<< "X wins:" << xWins <<endl;
+      cout<< "O wins:" << oWins <<endl;
+      cout<< endl;
+	 
   }
   return 0;
 }
@@ -57,9 +76,9 @@ void printboard (int table[3][3]) {
     for(int j =0;j<3; j++) {
       if(table[i][j] == 0) {
         cout << "  ";
-      }else if (table[i][j] == 0){
+      }else if (table[i][j] == 1){
         cout << "X" << " ";
-      } else if (table[i][j] == 1) {
+      } else if (table[i][j] == 2) {
         cout << "O" << " ";
       }
     }
@@ -68,32 +87,32 @@ void printboard (int table[3][3]) {
 }
 
 bool checkwin(int player, int board[3][3]) {
-  //Verticals
-  if(board[2][0]==player && board [2][1] && board [2][2] == player) {
+ //Verticals
+ if(board[2][0]==player && board [2][1] && board [2][2] == player) {
     return true;
-  }
+ }
  if(board[1][0]==player && board [1][1] && board [1][2] == player) {
     return true;
-}
-if(board[0][0]==player && board [0][1] && board [0][2] == player) {
-  return true;
-}
-//Diagonals
-if(board[0][0]==player && board [1][1] && board [2][2] == player) {
-  return true;
-}
-if(board[2][0]==player && board [2][1] && board [2][2] == player) {
-  return true;
-}
-//Horizantals
-if(board[0][0]==player && board [1][0] && board [2][0] == player) {
-  return true;
-}
-if(board[0][1]==player && board [1][1] && board [2][1] == player) {
-  return true;
-}
-if(board[0][2]==player && board [1][2] && board [2][2] == player) {
-  return true;
-}
+ }
+ if(board[0][0]==player && board [0][1] && board [0][2] == player) {
+   return true;
+ }
+ //Diagonals
+ if(board[0][0]==player && board [1][1] && board [2][2] == player) {
+   return true;
+ }
+ if(board[2][0]==player && board [2][1] && board [2][2] == player) {
+   return true;
+ }
+ //Horizantals
+ if(board[0][0]==player && board [1][0] && board [2][0] == player) {
+   return true;
+ }
+ if(board[0][1]==player && board [1][1] && board [2][1] == player) {
+   return true;
+ }
+ if(board[0][2]==player && board [1][2] && board [2][2] == player) {
+   return true;
+ }
  return false;
 }
