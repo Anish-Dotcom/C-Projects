b09773b024a00822412e3e6c9930e664b8281d8e
U-BSD\460470 <460470@SHS-BYBF6X3.beaverton.k12.or.us>
2025-10-20 11:40:26 -0700

    Studentlist

--- Studentlist/Studentlist.cpp
+++ Studentlist/Studentlist.cpp
@@ -9,13 +9,25 @@ struct Student {
   float gpa;
 };
 
+void add(vector<Student*>& Studentlist);
+
+
 int main() {
-  vector<Student> Studentlist;
+  vector<Student*> Studentlist;
   Student George;
   strcpy (George.name, "George");
   George.id = 123213;
   George.gpa = 4.23;
-  Studentlist[0] = George;
-  cout << Studentlist[0] << endl;
+  add(Studentlist);
+  Student* s = Studentlist[0];
+  cout<< s->id << endl;
+  cout<< s->gpa << endl;
   return 0;
+}
+void add(vector<Student*>& Studentlist) {
+  Student* s = new Student();
+  (*s).id = 123123;
+  (*s).gpa = 4.12;
+  strcpy((*s).name, "Name");
+  Studentlist.push_back(s);
 }
