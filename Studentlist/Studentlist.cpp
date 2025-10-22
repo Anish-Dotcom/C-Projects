11a6ef6f23138ade61f8d4f10aa455453bddd993
U-BSD\460470 <460470@SHS-BYBF6X3.beaverton.k12.or.us>
2025-10-22 11:57:44 -0700

    Studentlist 4

--- Studentlist/Studentlist.cpp
+++ Studentlist/Studentlist.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 struct Student {
-  char name[10];
+  char firstname[10];
+  char lastname[20];
   int id;
   float gpa;
 };
@@ -14,20 +15,25 @@ void add(vector<Student*>& Studentlist);
 
 int main() {
   vector<Student*> Studentlist;
-  Student George;
-  strcpy (George.name, "George");
-  George.id = 123213;
-  George.gpa = 4.23;
   add(Studentlist);
   Student* s = Studentlist[0];
-  cout<< s->id << endl;
-  cout<< s->gpa << endl;
+  cout << s->firstname << ' ' << s->lastname << ", " << s->id << ", " << s->gpa << endl;
   return 0;
 }
 void add(vector<Student*>& Studentlist) {
   Student* s = new Student();
-  (*s).id = 123123;
-  (*s).gpa = 4.12;
-  strcpy((*s).name, "Name");
+  cout << "First name: ";
+  char name[20];
+  cin.get(name,20);
+  strcpy(s->firstname, name);
+  cin.ignore();
+  cout << "Last name: ";	 
+  cin.get(name, 20);
+  strcpy(s->lastname, name);
+  cin.ignore();
+  cout<< "Type id: ";
+  cin >> s->id;
+  cout << "Type GPA: ";
+  cin >> s->gpa;
   Studentlist.push_back(s);
 }
